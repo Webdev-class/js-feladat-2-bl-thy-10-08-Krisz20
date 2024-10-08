eeeb09cec6931b1a47620e2572f31550e264d835
Késmárki Krisztián <kesmarki.krisztian@gmail.com>
2024-10-08 14:58:27 +0200

    f1.js

--- f1/f1.js
+++ f1/f1.js
@@ -1,7 +1,17 @@
 
 const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
+
+function atlagSzamitas(tomb){
+    let osszeg = 0;
+    for(let i = 0; i < tomb.length; i++){
+        osszeg += tomb[i];
+    }
+    return Math.floor(osszeg / tomb.length);
+}
+
 async function f1 (lat, long, timeStr) {
-    //TODO
+    const response = await fetch(APIAdress + `latitude=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m&timezone=auto`).then(response => response.json());
+    return atlagSzamitas(response.hourly.temperature_2m);
 }
 
 module.exports = f1;
